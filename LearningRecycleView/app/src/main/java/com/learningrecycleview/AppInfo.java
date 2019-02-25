0bc98086ed8fdc0019089263adb3d91179987fda
AlessandraMayumi <alessandra_mms@hotmail.com>
2019-02-24 21:38:42 -0300

    clear

--- LearningRecycleView/app/src/main/java/com/learningrecycleview/AppInfo.java
+++
@@ -1,9 +0,0 @@
-package com.learningrecycleview;
-
-import android.graphics.drawable.Drawable;
-
-public class AppInfo {
-    CharSequence label;
-    CharSequence packageName;
-    Drawable icon;
-}
