3403558cb14b16e886f62116c2cbc5bf5a71ade4
amayumi <alessandra_mms@hotmail.com>
2019-05-10 18:29:03 -0300

    Relogio de Cabeceira

--- RelogioDeCabeceira/app/src/main/java/com/devmasterteam/relogiodecabeceira/MainActivity.java
+++ RelogioDeCabeceira/app/src/main/java/com/devmasterteam/relogiodecabeceira/MainActivity.java
@@ -1,10 +1,17 @@
 package com.devmasterteam.relogiodecabeceira;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
@@ -12,11 +19,20 @@ import java.util.Calendar;
 import java.util.Locale;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ViewHolder mViewHolder = new ViewHolder();
     private Handler mHandler = new Handler();
     private Runnable mRunnable;
     private boolean mRunnableStopped = false;
+    private boolean mIsBatteryOn = true;
+
+    private BroadcastReceiver mBatteryReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            mViewHolder.mTextBatteryLevel.setText(String.valueOf(level) + "%");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +42,18 @@ public class MainActivity extends AppCompatActivity {
 
         this.mViewHolder.mTextHourMinute = (TextView) this.findViewById(R.id.text_hour_minute);
         this.mViewHolder.mTextSecond = (TextView) this.findViewById(R.id.text_second);
+        this.mViewHolder.mTextBatteryLevel = (TextView) this.findViewById(R.id.text_battery_level);
         this.mViewHolder.mCheckBattery = (CheckBox) this.findViewById(R.id.check_battery);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        this.registerReceiver(this.mBatteryReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        this.mViewHolder.mCheckBattery.setChecked(true);
+
+        this.setListener();
+
     }
 
     @Override
@@ -47,6 +74,28 @@ public class MainActivity extends AppCompatActivity {
         Log.d("stop=", "stop");
     }
 
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.check_battery) {
+            this.toggleCheckBattery();
+        }
+    }
+
+    private void toggleCheckBattery() {
+        if (this.mIsBatteryOn) {
+            this.mIsBatteryOn = false;
+            this.mViewHolder.mTextBatteryLevel.setVisibility(View.GONE);
+        } else {
+            this.mIsBatteryOn = true;
+            this.mViewHolder.mTextBatteryLevel.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void setListener() {
+        this.mViewHolder.mCheckBattery.setOnClickListener(this);
+    }
+
     private void startBedside() {
         final Calendar calendar = Calendar.getInstance();
 
@@ -59,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 Log.d("run=", "run");
                 calendar.setTimeInMillis(System.currentTimeMillis());
-                String hourMinuteFormat = String.format(Locale.getDefault(),"%02d:%02d",
+                String hourMinuteFormat = String.format(Locale.getDefault(), "%02d:%02d",
                         calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE));
                 String secondFormat = String.format(Locale.getDefault(), "%02d",
                         calendar.get(Calendar.SECOND));
@@ -79,6 +128,7 @@ public class MainActivity extends AppCompatActivity {
     private static class ViewHolder {
         TextView mTextHourMinute;
         TextView mTextSecond;
+        TextView mTextBatteryLevel;
         CheckBox mCheckBattery;
     }
 }
