c032b0c48a9b84d242644e19c8376f729b2744d4
amayumi <alessandra_mms@hotmail.com>
2019-05-20 22:37:50 -0300

    Phototicker

--- FestaFimAno/app/src/main/java/com/devmasterteam/festafimano/view/MainActivity.java
+++ FestaFimAno/app/src/main/java/com/devmasterteam/festafimano/view/MainActivity.java
@@ -69,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return dayDecember31 - today;
     }
 
+<<<<<<< Updated upstream
     private void verifyPresence() {
         String presence = this.mSecurityPreferences.getStoredString(FimDeAnoConstants.PRESENCE);
         if (presence.equals("")) {
@@ -80,6 +81,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+||||||| merged common ancestors
+    private void verifyPresence() {
+        String presence = this.mSecutiryPreferences.getStoredString(FimDeAnoConstants.PRESENCE);
+        if (presence.equals("")) {
+            this.mViewHolder.buttonConfirm.setText(R.string.nao_confirmado);
+        } else if (presence.equals(FimDeAnoConstants.CONFIRMED_WILL_GO)) {
+            this.mViewHolder.buttonConfirm.setText(R.string.sim);
+        } else if (presence.equals(FimDeAnoConstants.CONFIRMED_WONT_GO)) {
+            this.mViewHolder.buttonConfirm.setText(R.string.nao);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.button_confirm) {
+            String presence = this.mSecutiryPreferences.getStoredString(FimDeAnoConstants.PRESENCE);
+            Intent intent = new Intent(this, DetailsActivity.class);
+            intent.putExtra(FimDeAnoConstants.PRESENCE, presence);
+            startActivity(intent);
+        }
+    }
+
+=======
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.button_confirm) {
+            String presence = this.mSecutiryPreferences.getStoredString(FimDeAnoConstants.PRESENCE);
+            Intent intent = new Intent(this, DetailsActivity.class);
+            intent.putExtra(FimDeAnoConstants.PRESENCE, presence);
+            startActivity(intent);
+        }
+    }
+
+    private void verifyPresence() {
+        String presence = this.mSecutiryPreferences.getStoredString(FimDeAnoConstants.PRESENCE);
+        if (presence.equals("")) {
+            this.mViewHolder.buttonConfirm.setText(R.string.nao_confirmado);
+        } else if (presence.equals(FimDeAnoConstants.CONFIRMED_WILL_GO)) {
+            this.mViewHolder.buttonConfirm.setText(R.string.sim);
+        } else if (presence.equals(FimDeAnoConstants.CONFIRMED_WONT_GO)) {
+            this.mViewHolder.buttonConfirm.setText(R.string.nao);
+        }
+    }
+
+>>>>>>> Stashed changes
     private static class ViewHolder {
         TextView textToday;
         TextView textDaysLeft;
---
+++ photicker/app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
@@ -0,0 +1,61 @@
+package com.devmasterteam.photicker.views;
+
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.devmasterteam.photicker.R;
+import com.devmasterteam.photicker.utils.ImageUtil;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        List<Integer> mListImage = ImageUtil.getImagesList();
+
+        final RelativeLayout relativeLayout = (RelativeLayout) this.findViewById(R.id.relative_photo_content_draw);
+        final LinearLayout content = (LinearLayout) this.findViewById(R.id.linear_horizontal_scroll_content);
+
+        for (Integer imageId : mListImage) {
+            ImageView image = new ImageView(this);
+
+            image.setImageBitmap(ImageUtil.decodeSampledBitmapFromResource(getResources(), imageId, 70, 70));
+            image.setPadding(20, 10, 20, 10);
+
+            BitmapFactory.Options dimensions = new BitmapFactory.Options();
+            dimensions.inJustDecodeBounds = true;
+            BitmapFactory.decodeResource(getResources(), imageId, dimensions);
+
+            final int width = dimensions.outWidth;
+            final int height = dimensions.outHeight;
+
+            image.setOnClickListener(onClickImageOption(relativeLayout, imageId, width, height));
+
+            content.addView(image);
+        }
+    }
+
+    private View.OnClickListener onClickImageOption(final RelativeLayout relativeLayout, final Integer imageId, int width, int height) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ImageView image = new ImageView(MainActivity.this);
+                image.setBackgroundResource(imageId);
+                relativeLayout.addView(image);
+            }
+        };
+    }
+}
